fe0c941c9dce0a5fab68f6bb5324928a8d7bfb5e
bbking <b@bbki.ng>
2025-01-15 10:11:26 +0000

    try read json

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const github = require('@actions/github');
 try {
   const nameToGreet = core.getInput('who-to-greet');
   console.log(`Hello ${nameToGreet}!`);
+
+  const data = fs.readFileSync('plugin.json', 'utf8');
+  const jsonData = JSON.parse(data);
+
+  // 输出 JSON 数据
+  console.log(jsonData);
 } catch (error) {
   core.setFailed(error.message);
 }
+
+

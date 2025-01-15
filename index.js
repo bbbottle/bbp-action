ac67aeefd4bbf1219c8de549a0ba14eb76138b65
bbking <b@bbki.ng>
2025-01-15 15:53:59 +0000

    chore: test list

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const github = require('@actions/github');
 const {DefaultArtifactClient} = require('@actions/artifact')
 const fs = require('fs');
 const path = require('path');
+const { exec } = require("child_process");
 
 const getArtifactId = async (artifactClient, artifactName) => {
   const artifactInfo = await artifactClient.getArtifact(artifactName);
@@ -21,12 +22,24 @@ const createDownloader = (artifactClient) => async (artifactName, fileName, enco
 
 async function run() {
   try {
-    const downloader = createDownloader(new DefaultArtifactClient());
-    const pluginData = await downloader('plugin-json', 'plugin.json', 'utf-8');
-    console.log(pluginData);
+    // const downloader = createDownloader(new DefaultArtifactClient());
+    // const pluginData = await downloader('plugin-json', 'plugin.json', 'utf-8');
+    // console.log(pluginData);
 
-    const wasmFile = await downloader('wasm-file', 'bbp_now.wasm', 'binary');
-    console.log(wasmFile);
+    // const wasmFile = await downloader('wasm-file', 'bbp_now.wasm', 'binary');
+
+    // exec ls -la
+    exec("ls -la", (error, stdout, stderr) => {
+      if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+      }
+      if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+      }
+      console.log(`stdout: ${stdout}`);
+    });
 
   } catch (error) {
     core.setFailed(`Action failed with error: ${error.message}`);

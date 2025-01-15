a25f7c49c7216276a3bc54999bcde4c27025c67b
bbking <b@bbki.ng>
2025-01-15 09:49:25 +0000

    chore: test changes

--- dist/index.js
+++ dist/index.js
@@ -60,12 +60,8 @@ const core = __nccwpck_require__(878);
 const github = __nccwpck_require__(842);
 
 try {
-  // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
   console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
 } catch (error) {
   core.setFailed(error.message);
 }

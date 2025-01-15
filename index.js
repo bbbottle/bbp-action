94dac1347eb18bb9ff21bc5006fe5d8fdb652706
bbking <b@bbki.ng>
2025-01-15 15:38:20 +0000

    fix: file reading

--- index.js
+++ index.js
@@ -9,24 +9,24 @@ const getArtifactId = async (artifactClient, artifactName) => {
   return artifactInfo?.artifact.id;
 }
 
-const createDownloader = (artifactClient) => async (artifactName, encoding) => {
+const createDownloader = (artifactClient) => async (artifactName, fileName, encoding) => {
     const artifactId = await getArtifactId(artifactClient, artifactName);
     const downloadPath = path.join(__dirname, 'artifacts');
     const downloadResponse = await artifactClient.downloadArtifact(artifactId, {
       path: downloadPath
     });
 
-    return fs.readFileSync(downloadResponse.downloadPath, encoding);
+    return fs.readFileSync(path.join(downloadResponse.downloadPath, fileName), encoding);
 }
 
 async function run() {
   try {
     const downloader = createDownloader(new DefaultArtifactClient());
-    const pluginData = await downloader('plugin-json', 'utf-8');
+    const pluginData = await downloader('plugin-json', 'plugin.json', 'utf-8');
     console.log(pluginData);
 
-    const wasmFile = await downloader('wasm-file');
-    console.log(wasmFile.buffer);
+    const wasmFile = await downloader('wasm-file', 'bbp_now.wasm', 'binary');
+    console.log(wasmFile);
 
   } catch (error) {
     core.setFailed(`Action failed with error: ${error.message}`);

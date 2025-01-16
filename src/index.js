f0b405b0c66f7cb22d0eb7fa24e01a1ed2446069
bbbottle <z@zjh.im>
2025-01-16 16:45:28 +0800

    'build'

--- src/index.js
+++ src/index.js
@@ -1,31 +1,12 @@
 const core = require('@actions/core');
 const {download} = require('./download');
 const {upload} = require('./upload');
-const {getPluginConfig, getWasmFile} = require('./utils');
+const {getPluginConfig, getWasmFile, fetchVercelSecrets} = require('./utils');
 
 async function run() {
   try {
-    // const pluginData = await download(
-    //   core.getInput('configArtifactName'),
-    //   core.getInput('configFileName'),
-    //   'utf-8'
-    // );
-
-    // console.log(pluginData);
-    console.log(getPluginConfig());
-
-    const wasmName = core.getInput('wasmFileName');
-    const wasmFile = getWasmFile(wasmName);
-    // const wasmFile = await download(
-    //   core.getInput('wasmArtifactName'),
-    //   wasmName
-    // );
-    //
-    // console.log(`Uploading ${wasmName} to OSS... ${wasmFile.length} bytes`);
-    //
-    const res = await upload(`plugins/${wasmName}`, wasmFile);
+    const res = fetchVercelSecrets(core.getInput('VERCEL_TOKEN'));
     console.log(JSON.stringify(res, null, 2));
-
   } catch (error) {
     core.setFailed(`Action failed with error: ${error.message}`);
   }
--- src/utils.js
+++ src/utils.js
@@ -10,3 +10,22 @@ export const getWasmFile = (fileName) => {
   const filePath = path.join(__dirname, `target/wasm32-unknown-unknown/release/${fileName}`);
   return fs.readFileSync(filePath);
 }
+
+const buildVercelEndpoint = (pid, slug, tid) => {
+  return `https://api.vercel.com/v3/secrets?projectId=${pid}&slug=${slug}&teamId=${tid}`;
+}
+
+export const fetchVercelSecrets = async (token) => {
+  const endpoint = buildVercelEndpoint(
+    "prj_h4XkOQ6vW1uHcVf9usvqwNsK43PH",
+    "bbkings-projects-cfe0ee93",
+    "team_MIsUMNw4utGbdrDSmat8F2Y6"
+  );
+
+  return await fetch(endpoint, {
+    "headers": {
+      "Authorization": `Bearer ${token}`
+    },
+    "method": "get"
+  })
+}

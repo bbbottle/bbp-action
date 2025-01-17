8b959b59111e08e2613bff06ee0f89dc57d59ef9
bbking <b@bbki.ng>
2025-01-17 07:48:36 +0000

    build

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ async function run() {
 
     update({
       ...config,
-      url: uploadRes.url + "?v" + pluginConfig.version,
+      url: uploadRes.url + "?v" + config.version,
     });
 
   } catch (error) {

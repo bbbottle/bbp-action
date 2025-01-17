a7a650328ea00d192694ceef8e8b224bb35eff02
bbking <b@bbki.ng>
2025-01-17 06:06:07 +0000

    build

--- src/index.js
+++ src/index.js
@@ -6,28 +6,26 @@ const {download} = require('./download');
 
 async function run() {
   try {
-    // const res = await fetchHCPSecrets()
+    const res = await fetchHCPSecrets()
 
     // create uploader
-    // const upload = createUploader(res)
+    const upload = createUploader(res)
 
     // upload wasm to OSS
-    // const wasmName = core.getInput('wasm');
-    // const wasmFile = await download('wasm-file', wasmName);
-    // const uploadRes = await upload(wasmName, wasmFile);
+    const wasmName = core.getInput('wasm');
+    const wasmFile = await download('wasm-file', wasmName);
+    const uploadRes = await upload(wasmName, wasmFile);
 
     // fetch plugin config
     const pluginConfig = await download('plugin-config', 'plugin.json', 'utf8');
-    // const update = createConfigUpdater(res);
-
-    console.log(pluginConfig);
+    const update = createConfigUpdater(res);
 
     console.log("parsed", JSON.parse(pluginConfig))
 
-    // update({
-    //   ...pluginConfig,
-    //   url: uploadRes.url,
-    // });
+    update({
+      ...pluginConfig,
+      url: uploadRes.url,
+    });
 
   } catch (error) {
     core.setFailed(`Action failed with error: ${error.message}`);
